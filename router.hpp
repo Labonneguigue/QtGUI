e6b040045d7cb4864a0aed7e5710d1ebb7f1ee37
Labonneguigue <granie.guillaume@gmail.com>
2016-12-21 11:59:49 +0100

    les .hpp

---
+++ router.hpp
@@ -0,0 +1,22 @@
+#ifndef ROUTER_H
+#define ROUTER_H
+
+#define PY_router "routing"
+
+
+
+/********************************/
+/*								*/
+/*		        ROUTER		    */
+/*								*/
+/********************************/
+
+class Router{
+
+public:
+    std::vector<double> getpath(double A ,double B);
+};
+
+
+
+#endif
